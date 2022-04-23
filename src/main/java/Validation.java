183a253e4cd525b73c9d3be36b473ead809cd894
Jack Burnett <jackjburnett@gmail.com>
2022-04-23 21:47:26 +0100

    Added file error messages

--- src/main/java/Validation.java
+++ src/main/java/Validation.java
@@ -3,13 +3,20 @@ import com.google.common.io.Files;
 import java.io.File;
 
 public class Validation {
-    public boolean FileValidator(String filePath){
-        if(new File(filePath).exists()){
-            //System.out.println(Files.getFileExtension(filePath));
-            if(Files.getFileExtension(filePath).equals("java")) {
+    public boolean FileValidator(String filePath) {
+        if (new File(filePath).exists()) {
+
+            if (Files.getFileExtension(filePath).equals("java")) {
                 return true;
             }
+            else{
+                System.out.println("Specified file is not of type 'java'.");
+                return false;
+            }
+        } else{
+            System.out.println("File path does not exist, or lead to a file.");
+            return false;
         }
-        return false;
     }
 }
+
