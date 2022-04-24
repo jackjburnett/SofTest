10ba04761324f6e1c93a6e0148c685e9ae740a6d
Jack Burnett <jackjburnett@gmail.com>
2022-04-24 13:17:35 +0100

    Implemented Runtime Execution

--- src/main/java/JavaExec.java
+++ src/main/java/JavaExec.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class JavaExec {
@@ -7,10 +11,36 @@ public class JavaExec {
     public JavaExec(){
         System.out.println("Please input the file directory of your Java program for execution: ");
         String filePath = input.next();
-        System.out.println(validate.FileValidator(filePath));
+        if(validate.FileValidator(filePath)){
+            new JavaExec(filePath);
+        }
     }
 
     public JavaExec(String filePath){
-        System.out.println(validate.FileValidator(filePath));
+        try {
+            Process processCompile = Runtime.getRuntime().exec("javac "+filePath);
+        } catch (IOException e) {
+
+        }
+        Process processRun = null;
+        try {
+            processRun = Runtime.getRuntime().exec("java "+filePath);
+        } catch (IOException e) {
+
+        }
+        try {
+            readCMD(" stdout:", processRun.getInputStream());
+            readCMD(" stderr:", processRun.getErrorStream());
+        } catch (Exception e) {
+
+        }
+    }
+
+    private static void readCMD(String func, InputStream input) throws Exception{
+        String currLine = null;
+        BufferedReader CMDLine = new BufferedReader(new InputStreamReader(input));
+        while ((currLine = CMDLine.readLine()) != null) {
+            System.out.println(func + " " + currLine);
+        }
     }
 }
