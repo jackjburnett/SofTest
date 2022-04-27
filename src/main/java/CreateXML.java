576e1fb40a73213750672111562c96125641e6a7
Jack Burnett <jackjburnett@gmail.com>
2022-04-28 00:39:53 +0100

    Added JAXB dependency

--- src/main/java/CreateXML.java
+++ src/main/java/CreateXML.java
@@ -1,11 +1,17 @@
 import java.util.Scanner;
+import java.io.File;
+import java.io.StringWriter;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 
 public class CreateXML {
     static Validation validate = new Validation();
     Scanner input = new Scanner(System.in);
     // This will not accept maven projects!
     public CreateXML(){
-        System.out.println("Please input the file directory of your XML for parsing: ");
+        System.out.println("Please input the file directory of your Java file for XML creation: ");
         String filePath = input.next();
         if (validate.XMLValidator(filePath)){
             new CreateXML(filePath);
