6d1649b854b6ef186073caeb9c460cb2216b2e57
Jack Burnett <jackjburnett@gmail.com>
2022-05-04 13:43:10 +0100

    Added TestViewer methods to be added

--- src/main/java/TestViewer.java
+++ src/main/java/TestViewer.java
@@ -1,3 +1,18 @@
-public class TestViewer {
+import java.io.File;
+
+public final class TestViewer {
+
+    public static void OpenTests(){
+
+    }
+
+    public static String SimplifyTest(String testResult){
+        return null;
+    }
+
+    public static File OpenFile(String filePath){
+        return null;
+    }
+
 
 }
